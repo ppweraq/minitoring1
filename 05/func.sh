e57c3546103cb75ad892cd9db6105a952d3c94ee
ppweraq <saakhaaya@fedora.(none)>
2023-09-24 18:35:10 +0300

    done 02

--- 05/func.sh
+++ 05/func.sh
@@ -4,19 +4,15 @@ number_of_folders (){
 sudo find "$1" -type d | wc -l
 }
 
-top_folders() {
+max_size_folders() {
  sudo du -h "$1" | sort -hr | head -n 5 | awk '{print NR " - " $2 ", " $1}'
 }
 
-get_file_count() {
+count_of_file() {
   sudo find "$1" -type f | wc -l
 }
 
-get_directory_size() {
-  sudo du -sb "$1" | awk '{print $1}'
-}
-
-get_file_count_by_extension() {
+count_of_extension_files() {
   sudo find "$1" -type f -name "*.$2" | wc -l
 }
 
@@ -28,15 +24,14 @@ symbol_links(){
     sudo find "$1" -type l | wc -l
 }
 
-
-
-get_top_files() {
-# Используем find для поиска файлов в указанной директории и её поддиректориях
-# Затем используем du для получения размеров файлов, и sort для сортировки по размеру в обратном порядке
-# Наконец, head выбирает первые 10 результатов, и awk форматирует вывод
-sudo find "$1" -type f -exec du -sh {} + | sort -hr | head -n "$2" | awk '{printf "%d - %s, %sB\n", $1, $3, $2}'  
+get_directory_size() {
+  sudo du -sb "$1" | awk '{print $1}'
 }
 
-get_top_executables() {
-sudo find "$1" -type f -executable -exec du -sh {} + | sort -hr | head -n "$2" | awk '{cmd = "md5sum " $3; cmd|getline result; printf "%d - %s, %sB, %s\n", $1, $3, $2, result}' | awk '{print $1,$2,$3,$4,$5}'
+top_files () {
+sudo find "$1" -type f -exec du -sh {} + | sort -hr | head -n "$2" | awk '{split($2, a, "."); printf "%d - %s, %sB, %s\n", NR, $2, $1, a[length(a)]}'
+}
+
+top_exec () {
+sudo find "$1" -type f -executable -exec du -sh {} + | sort -hr | head -n "$2" | awk '{cmd = "md5sum " $2; cmd|getline result; printf "%d - %s, %sB, %s\n", NR, $2, $1, result}' | awk '{print $1,$2,$3,$4,$5}'
 }
--- 05/main.sh
+++ 05/main.sh
@@ -21,19 +21,19 @@ source ./func.sh
 start_timer=$(date +%s)
 echo "Total number of folders (including all nested ones) = $(number_of_folders "$1")"
 echo "Top 5 folders of maximum size arranged in descending order (path and size):
-$(top_folders "$1" 5)"
-echo "Total number of files = $(get_file_count "$1")"
+$(max_size_folders "$1" 5)"
+echo "Total number of files = $(count_of_file "$1")"
 echo "Number of:"
-echo "Configuration files (with the .conf extension) = $(get_file_count_by_extension "$1" "conf")"
-echo "Text files = $(get_file_count_by_extension "$1" "txt")"
-echo "Executable files = $(get_file_count_by_extension "$1" "exe")"
-echo "Log files (with the extension .log) = $(get_file_count_by_extension "$1" "log")"
+echo "Configuration files (with the .conf extension) = $(count_of_extension_files "$1" "conf")"
+echo "Text files = $(count_of_extension_files "$1" "txt")"
+echo "Executable files = $(count_of_extension_files "$1" "exe")"
+echo "Log files (with the extension .log) = $(count_of_extension_files "$1" "log")"
 echo "Archive files = $(archive_file "$1")"
 echo "Symbolic links = $(symbol_links "$1")"
 echo "Top 10 files of maximum size arranged in descending order (path, size and type): 
-$(get_top_files "$1" 10)"
+$(top_files "$1" 10)"
 echo "Top 10 executable files of the maximum size arranged in descending order (path, size and MD5 hash of file):
-$(get_top_executables "$1" 10)"
+$(top_exec "$1" 10)"
 end_timer=$(date +%s) 
 timer_amount=$(($end_timer - $start_timer))
-echo "Script execution time (in seconds) = $timer_amount"
+echo "Script execution time (in seconds) = $timer_amount sec"
